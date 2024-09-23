a19d9892d01e69dd010f1c8e2fddc2ec05e54cfb
superXnova21 <107920527+superXnova21@users.noreply.github.com>
2024-09-23 22:56:30 +0600

    Create graphInput.cpp

---
+++ graphInput.cpp
@@ -0,0 +1,19 @@
+struct Graph {
+    int V;
+    vector<vector<pair<int, int>>> adj;  
+
+    // Initialize like - Graph g(vertices)
+    Graph(int vertices) : V(vertices), adj(vertices + 1) {}
+
+    void addEdge(int u, int v, int w = 1, bool directed = false) {
+        adj[u].emplace_back(v, w);
+        if (!directed) adj[v].emplace_back(u, w);
+    }
+
+    void clear() {
+        for (auto& edges : adj) {
+            edges.clear();
+        }
+    }
+
+};
